d7eae930799185167aa235ab352c952b48b63fb1
ebiggz <erikbigler@gmail.com>
2020-02-27 01:32:18 -0600

    move axios config creation to function so its build after .env is read

--- src/bot/services/github-service.ts
+++ src/bot/services/github-service.ts
@@ -1,15 +1,17 @@
 import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 import { ICreateIssueRequest, IIssue, ICommitData, IGetCommitsRequest } from '../models/github';
 
-const defaultAxiosConfig: AxiosRequestConfig = {
-    auth: {
-        username: process.env.GITHUB_USER,
-        password: process.env.GITHUB_TOKEN,
-    },
-    headers: {
-        'User-Agent': 'crowbartools/crowbartools-discord-bot',
-    },
-};
+function getDefaultAxiosConfig(): AxiosRequestConfig {
+    return {
+        auth: {
+            username: process.env.GITHUB_USER,
+            password: process.env.GITHUB_TOKEN,
+        },
+        headers: {
+            'User-Agent': 'crowbartools/crowbartools-discord-bot',
+        },
+    };
+}
 
 export async function createIssue(createIssueRequest: ICreateIssueRequest): Promise<IIssue> {
     const createUrl = `https://api.github.com/repos/${createIssueRequest.repo}/issues`;
@@ -21,7 +23,7 @@ export async function createIssue(createIssueRequest: ICreateIssueRequest): Prom
 
     let response: AxiosResponse<IIssue>;
     try {
-        response = await axios.post(createUrl, body, defaultAxiosConfig);
+        response = await axios.post(createUrl, body, getDefaultAxiosConfig());
     } catch (error) {
         console.log(error);
     }
@@ -49,7 +51,7 @@ export async function searchIssues(repo: string, search: string): Promise<IIssue
 
     let response: AxiosResponse<IIssue[]>;
     try {
-        response = await axios.get<IIssue[]>(searchUrl, defaultAxiosConfig);
+        response = await axios.get<IIssue[]>(searchUrl, getDefaultAxiosConfig());
     } catch (error) {
         console.log('Error searching github issues', error);
     }
