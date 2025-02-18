b693f7a82dd5d8b9ae552b7b4fba6552dcdce189
Erik Bigler <erikbigler@gmail.com>
2025-02-17 20:42:24 -0600

    chore: fix typo in overlay issues topic

--- src/interactions/commands/handlers/info/topics/overlay-issues.ts
+++ src/interactions/commands/handlers/info/topics/overlay-issues.ts
@@ -13,7 +13,7 @@ If you're experiencing issues with videos and audio in your Firebot Overlay, fol
     - Scroll down to the bottom; Sources.
     - Disable "Enable Browser Source Hardware Acceleration"
     - Check results in OBS after restarting OBS.
-2. If that doesn't hel, you can also try disabling GPU compositing for your browser source by following the steps [here](https://gist.github.com/akarak/9b1d7e8731b30b7daa3b640e8cb413b5).
+2. If that doesn't help, you can also try disabling GPU compositing for your browser source by following the steps [here](https://gist.github.com/akarak/9b1d7e8731b30b7daa3b640e8cb413b5).
 
 If none of these options work, you might need to check the encoding of your video-file.
 `),
