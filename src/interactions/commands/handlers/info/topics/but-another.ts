1c0ecd8625c6b538266699896ccc05c63e9d7cda
CKY- <xx_cky_xx@hotmail.com>
2025-04-13 22:05:37 -0600

    feat: make BABDI a little less harsh

--- src/interactions/commands/handlers/info/topics/but-another.ts
+++ src/interactions/commands/handlers/info/topics/but-another.ts
@@ -6,8 +6,8 @@ export const butAnother: InfoTopic = {
         embeds: [
             getBaseEmbed().setTitle('But Another Bot Does It?').setDescription(`
 Just a friendly reminder, just because Software A supports something, that doesn't mean that Software B is required to support it.
-Things you want won't always appear in the software you use.
-This is true in all walks of computer life and why there are so many tools that do the same thing.
+Things you want may not always appear in the software you use, or may take time from our open source community to contribute and build.
+This is true in all walks of computer life and why there are so many tools that do similar things - if Firebot isn't able to do something today, there might be another tool out there that can be a stopgap if/until Firebot supports it.
 `),
         ],
     },
